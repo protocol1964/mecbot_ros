7397cceab51c59ea81bbaef515db8773289292e1
Sota Watanabe <shu.cyclone@gmail.com>
2019-05-07 21:27:21 +0900

    送受信部分を書き直した

--- mecbot.py
+++ mecbot.py
@@ -4,12 +4,28 @@ import serial
 from std_msgs.msg import Int32
 from geometry_msgs.msg import Twist
 
+def write_command(cmd):
+    ser.write(cmd + "\n")
+    return read_command()
+
+def read_command():
+    return_str = ""
+    while True:
+        recv_char = ser.read()
+        return_str += recv_char
+        if recv_char == ">":
+            break
+    return return_str
+
 def cmd_vel_callback(msg):
-    ser.write("VCX" + str(msg.linear.x) + "\r\n")
-    ser.write("VCR" + str(msg.angular.z) + "\r\n")
+    print(write_command("VCX" + str(msg.linear.x)))
+    print(write_command("VCR" + str(msg.angular.z)))
 
 rospy.init_node("mecbot")
 cmd_vel_sub = rospy.Subscriber("cmd_vel", Twist, cmd_vel_callback)
+
 ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=0)
+ser.reset_input_buffer()
+ser.reset_output_buffer()
 
 rospy.spin()
